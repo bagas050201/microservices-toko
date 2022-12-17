1d2d338b77eb526b7fa467c67eab881561cd275a
Muhammad Bagas Pradana <54340164+bagas050201@users.noreply.github.com>
2022-12-17 17:51:04 +0700

    Delete data.go

--- models/data.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type informasi_toko struct {
-	//beritahu ke gorm bahwa id adalah primary key
-	Id_toko          int64  `gorm:"primaryKey" json:"id"`
-	nama_toko string `gorm:"type:varchar(300)" json:"nama_toko"`
-	deskripsi  string `gorm:"type:text" json:"deskripsi"`
-	jumlah_product  string `gorm:"type:text" json:"jumlah_product"`
-}
